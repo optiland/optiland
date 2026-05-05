7ee0d0e774be86073c34ab9b44eff36cdc9e2728
gustavu92 <vasconcelos.gjq@gmail.com>
2026-05-05 16:58:03 +0200

    Fix Torch autograd for Zernike optimization

--- optiland/optimization/variable/zernike_coeff.py
+++ optiland/optimization/variable/zernike_coeff.py
@@ -11,8 +11,7 @@ drpaprika, 2025
 
 from __future__ import annotations
 
-import numpy as np
-
+import optiland.backend as be
 from optiland.optimization.scaling.identity import IdentityScaler
 from optiland.optimization.variable.polynomial_coeff import PolynomialCoeffVariable
 
@@ -50,48 +49,38 @@ class ZernikeCoeffVariable(PolynomialCoeffVariable):
 
         Returns:
             float: The current value of the Zernike coefficient.
-
         """
         surf = self._surfaces[self.surface_number]
         i = self.coeff_index
-        try:
-            value = surf.geometry.coefficients[i]
-        except IndexError:
-            pad_width_i = max(0, i + 1)
-            c_new = np.pad(
-                surf.geometry.coefficients,
-                pad_width=(0, pad_width_i),
-                mode="constant",
-                constant_values=0,
-            )
-            surf.geometry.coefficients = c_new
-            value = 0
-        return value
+        coeffs = surf.geometry.coefficients
+
+        if i < len(coeffs):
+            return coeffs[i]
+
+        pad_width = i + 1 - len(coeffs)
+        padding = be.array([0.0] * pad_width)
+        surf.geometry.coefficients = be.concatenate([coeffs, padding])
+
+        return surf.geometry.coefficients[i]
 
     def update_value(self, new_value):
         """Update the value of the Zernike coefficient.
 
         Args:
             new_value (float): The new value of the Zernike coefficient.
-
         """
         surf = self.optic.surfaces[self.surface_number]
         i = self.coeff_index
+        coeffs = surf.geometry.coefficients
 
-        if i < len(surf.geometry.coefficients):
-            # If the coefficient already exists, update it
-            surf.geometry.coefficients[i] = new_value
-        else:
-            # If the coefficient does not exist, pad the array and set the value
-            pad_width_i = max(0, i + 1)
-            new_coefficients = np.pad(
-                surf.geometry.coefficients,
-                pad_width=(0, pad_width_i),
-                mode="constant",
-                constant_values=0,
-            )
-            new_coefficients[i] = new_value
-            surf.geometry.coefficients = new_coefficients
+        if i >= len(coeffs):
+            pad_width = i + 1 - len(coeffs)
+            padding = be.array([0.0] * pad_width)
+            coeffs = be.concatenate([coeffs, padding])
+
+        surf.geometry.coefficients = be.stack(
+            [new_value if j == i else coeff for j, coeff in enumerate(coeffs)]
+        )
 
     def __str__(self):
         """Return a string representation of the variable.
